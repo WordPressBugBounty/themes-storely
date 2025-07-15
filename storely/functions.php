a5d6db3625af23e4a2ec9ea3190e488c0715395e
Sean Murphy <sean@iamseanmurphy.com>
2025-07-15 08:21:04 +0000

    21.0

--- storely/functions.php
+++ storely/functions.php
@@ -5,7 +5,7 @@ function storely_setup() {
 /**
  * Define Theme Version
  */
-define( 'STORELY_THEME_VERSION', '20.9' );
+define( 'STORELY_THEME_VERSION', '21.0' );
 
 // Root path/URI.
 define( 'STORELY_PARENT_DIR', get_template_directory() );
